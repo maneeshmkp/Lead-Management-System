2c74d60308e2b0c248ad79600dd4d6a4653a4e9f
Maneesh Kumar <maneeshprajapati2004@gmail.com>
2025-08-23 13:25:06 +0530

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-5xl font-bold text-white drop-shadow-lg">
+        🚀 Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
