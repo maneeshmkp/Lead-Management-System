9bffe8d0f718612d44128c48a636a1f5b8e8b8d5
Maneesh Kumar <maneeshprajapati2004@gmail.com>
2025-08-24 16:03:56 +0530

    authRoutes.js last changes committed

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { register, login, me, logout } from "../controllers/authController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
+const router = Router();
+router.post("/register", register);
+router.post("/login", login);
+router.post("/me", authMiddleware, me);
+router.post("/logout", logout);
+
+export default router; 
